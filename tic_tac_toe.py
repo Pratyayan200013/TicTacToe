ccf73e86b172b765ed463e80afaebfd657432362
Pratyayan Sarkar <sarkarpratyayan@gmail.com>
2020-05-09 22:36:01 +0530

    Instructions to play the game are added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,10 +3,12 @@ from numpy import *
 arr=array([[0 for x in range(3)]for y in range(3)])
 print("Player1 will always go first, decide among yourself who will be Player1 and who will be Player2..Happy play..")
 print(arr)
+print("Read the instructions below")
 print("You CAN'T CHOOSE THE PLACES WHICH ARE ALREADY FILLED WITH 1'S AND 2'S !!!")
+ print("Give the index here you want to insert your value.. E.g- '0' enter '1' enter will insert your value in the position [0][1]")
 for z in range(10):
     a = input("Player1 or Player2: ")
-    if a=='Player1'.lower():
+    if a=='Player1'.lower() or a=='Player1':
         i=int(input(""))
         j=int(input(""))
         arr[i][j]=1
@@ -17,6 +19,7 @@ for z in range(10):
         arr[i][j]=2
         print("player1 turn")
     print("You CAN'T CHOOSE THE PLACES WHICH ARE ALREADY FILLED WITH 1'S AND 2'S !!!")
+   
 
     print(arr)
     if  arr[0][0]==1 and arr[0][1]==1 and arr[0][2]==1:
